af2b7c0ff5467ebb3bdc3b74a097c6f25c936a14
FedericoCantarelli <fede.cantarelli98@gmail.com>
2021-11-05 18:05:14 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The object of this project is to develop a process to predict customer satisfaction based on both historical opinion and the characteristics of the purchasing.',
+    author='Federico Cantarelli',
+    license='MIT',
+)
